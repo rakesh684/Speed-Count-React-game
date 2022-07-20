f8b6c8eb07547962ab448cbd76afbafba40a0254
Rakesh kumar <dr.rakesh684@gmail.com>
2022-07-20 15:57:48 +0530

    added useEffect and some function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import './App.css';
-import WordGame from './hooks/wordGame'
+// import WordGame from './hooks/wordGame'
 function App(){
-  const {text,handleChange}=WordGame()
+  const START_TIME=10
+  const [text,setText]=useState("")
+    const [timeRemaining,setTimeRemaining]=useState(START_TIME)
+    const [isTimeRunning,setIsTimeRunning]=useState(false)
+    const [wordCount,setWordCount]=useState(0)
+    function handleChange(e){
+      const {value}=e.target
+       setText(value)
+    }
+    
+    function calculateWordCount(text){
+      const wordsArr=text.trim().split(" ")
+      return wordsArr.filter(word=> word!=="").length 
+    }
+
+    function startClock(){
+      setIsTimeRunning(true)
+      setTimeRemaining(START_TIME)
+      setText("")
+      
+    }
+    
+    function endGame(){
+      setIsTimeRunning(false)
+      setWordCount(calculateWordCount(text))
+    }
+
+    useEffect(()=>{
+      if(isTimeRunning && timeRemaining>0){
+        setTimeout(()=>{
+          setTimeRemaining(time=>time-1)
+        },1000)
+      }
+      else if(timeRemaining===0){
+        endGame()
+      }
+    },[timeRemaining,isTimeRunning,])
+
   return(
     <div>
        <h1>How fast do use type?</h1>
       <br/>
       <textarea 
-         value={text}
          onChange={handleChange}
+         value={text}
+         
       />
-      <h4>Time remaining :</h4>
-      <button>Start</button>
-      <h1>Word Count</h1>
+      <h4>Time remaining :{timeRemaining}</h4>
+      <button onClick={startClock}>Start</button>
+      <h1>Word Count:{wordCount}</h1>
     </div>
   )
 }
--- src/hooks/wordGame.js
+++ src/hooks/wordGame.js
@@ -1,12 +1,29 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 function WordGame(){
-    const [text,setText]=useState(" ")
-    function handleChage(e){
+    const [text,setText]=useState("")
+    const [timeRemaining,setTimeRemaining]=useState(5)
+    const [isTimeRunning,setIsTimeRunning]=useState(false)
+    function handleChange(e){
         const {value}=e.target
         setText(value)
     }
+   function calculateWordCount(text){
+    const wordsArr=text.trim().split(" ")
+    return wordsArr.filter(word=> word!=="").length
+    
+   }
+   useEffect(()=>{
+    if(isTimeRunning===true && timeRemaining>0){
+        setTimeout(()=>{
+            setTimeRemaining(time=>time-1)
+        },1000)
+    }
+   },
+   [timeRemaining])
+   
+   
     return(
-        {text,handleChage}
+        {text,timeRemaining,handleChange,calculateWordCount}
            
     )
 }
