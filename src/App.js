c16085d7cd00620313d809995869ed57b6e24dbd
Rakesh kumar <dr.rakesh684@gmail.com>
2022-07-23 11:28:43 +0530

    added custom hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import React,{useEffect, useState,useRef} from "react";
+import React from "react";
 import './App.css';
-// import WordGame from './hooks/wordGame'
+import useWordGame from './hooks/useWordGame'
 function App(){
-  const START_TIME=10
-  const [text,setText]=useState("")
-    const [timeRemaining,setTimeRemaining]=useState(START_TIME)
-    const [isTimeRunning,setIsTimeRunning]=useState(false)
-    const [wordCount,setWordCount]=useState(0)
-    const textBoxRef = useRef(null)
-    function handleChange(e){
-      const {value}=e.target
-       setText(value)
-    }
-    
-    function calculateWordCount(text){
-      const wordsArr=text.trim().split(" ")
-      return wordsArr.filter(word=> word!=="").length 
-    }
-
-    function startGame(){
-      setIsTimeRunning(true)
-      setTimeRemaining(START_TIME)
-      setText("")
-      setWordCount(0)
-      textBoxRef.current.disabled = false
-      textBoxRef.current.focus()
-      
-    }
-    
-    function endGame(){
-      setIsTimeRunning(false)
-      setWordCount(calculateWordCount(text))
-    }
-
-    useEffect(()=>{
-      if(isTimeRunning && timeRemaining>0){
-        setTimeout(()=>{
-          setTimeRemaining(time=>time-1)
-        },1000)
-      }
-      else if(timeRemaining===0){
-        endGame()
-      }
-    },[timeRemaining,isTimeRunning,])
-
+  const {
+    text,
+    wordCount,
+    textBoxRef,
+    startGame,
+    timeRemaining,
+    isTimeRunning,
+    handleChange
+    }=useWordGame()
   return(
     <div>
        <h1>How fast do use type?</h1>
